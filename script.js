159644fe0b919a13de8576edb4d4a14520a0c346
aballinger1981 <andrew@andrewballinger.com>
2017-06-22 16:32:46 -0400

    Form submit works.  Implemented alertifyjs and using formspree to send email.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   var enableScroll = true;
 
-  $(document).on("scroll", function () {
+  $(document).on('scroll', function () {
     if (enableScroll === true) {
       onScroll();
     }
@@ -18,9 +18,9 @@ $(document).ready(function () {
     var vph = $(window).innerHeight();
 
     if (vpw > 499 && vph > 1000) {
-      $(".full-page").css("height", vph - 100);
+      $('.full-page').css('height', vph - 100);
     } else {
-      $(".full-page").removeProp("height");
+      $('.full-page').removeProp('height');
     }
   }
 
@@ -28,39 +28,38 @@ $(document).ready(function () {
     var scrollPosition = $(document).scrollTop();
     $('nav a').each(function (index) {
       var currentLink = $(this);
-      var refElement = "#" + currentLink.attr("data-id");
+      var refElement = '#' + currentLink.attr('data-id');
       if ($(refElement).position().top <= scrollPosition + 100) {
-        $('nav li').removeClass("active");
-        currentLink.parent().addClass("active");
+        $('nav li').removeClass('active');
+        currentLink.parent().addClass('active');
       }
       else {
-        currentLink.parent().removeClass("active");
+        currentLink.parent().removeClass('active');
       }
     });
   }
 
-  $("ul li").click(function () {
+  $('ul li').click(function () {
     enableScroll = false;
-    console.log(enableScroll);
-    $("ul li.active").removeClass("active");
-    $("ul li").addClass("inactive");
+    $('ul li.active').removeClass('active');
+    $('ul li').addClass('inactive');
     var $this = $(this);
-    if (!$this.hasClass("active")) {
-      $this.removeClass("inactive");
-      $this.addClass("active");
+    if (!$this.hasClass('active')) {
+      $this.removeClass('inactive');
+      $this.addClass('active');
     }
     event.preventDefault();
   });
 
   $('.scroll-link').on('click', function () {
     event.preventDefault();
-    var sectionID = $(this).attr("data-id");
+    var sectionID = $(this).attr('data-id');
     scrollToID('#' + sectionID, 750);
   });
 
   $('#nav-toggle').on('click', function () {
     event.preventDefault();
-    $('#main-nav').toggleClass("open");
+    $('#main-nav').toggleClass('open');
   });
 
   //scroll function
@@ -71,10 +70,31 @@ $(document).ready(function () {
     $('html,body').animate({
       scrollTop: targetOffset
     }, speed);
-    if (mainNav.hasClass("open")) {
-      mainNav.css("height", "1px").removeClass("in").addClass("collapse");
-      mainNav.removeClass("open");
+    if (mainNav.hasClass('open')) {
+      mainNav.css('height', '1px').removeClass('in').addClass('collapse');
+      mainNav.removeClass('open');
     }
     setTimeout(() => enableScroll = true, 800);
   }
+
+  $('.contact').submit(function (event) {
+    var name = document.getElementById('inputName');
+    var email = document.getElementById('inputEmail');
+    var message = document.getElementById('inputMessage');
+    alertify.set('notifier', 'position', 'bottom-right');
+
+    if (!name.value || !email.value || !message.value) {
+      alertify.error('Please check your entries');
+    } else {
+      $.ajax({
+        url: "https://formspree.io/aballinger1981@gmail.com",
+        method: "POST",
+        data: $(this).serialize(),
+        dataType: "json"
+      });
+      $(this).get(0).reset();
+      alertify.success('Message sent', 5);
+    }
+    event.preventDefault();
+  });
 });
